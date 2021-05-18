d3f63525a4cf5c6735304bde83ca8f1c01d58999
BrendanFinnegan <bf289907@gmail.com>
2021-05-18 18:57:12 -0400

    commit tree b950079527ebe052d8d309f813ddbff4d6f2c9be

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const findMatching = (arry, name) => {
+    
+   let y = arry.filter(x => x.toLowerCase() === name.toLowerCase());
+   return y
+}
+
+const fuzzyMatch = (arry, name) => {
+    
+    let y = arry.filter(x => x[0] === name[0]);
+    return y
+ }
+ const matchName = (arry, name) => {
+    
+    let y = arry.filter(x => x.name === name);
+    return y
+ }
